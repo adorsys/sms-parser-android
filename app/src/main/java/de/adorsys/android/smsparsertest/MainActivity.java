84da0acfd2310bb72b3548a43a6c8b58a8b868b9
Drilon Reçica <drilonreqica@gmail.com>
2017-09-11 10:07:28 +0200

    Implemented pmd check suggestions

--- app/src/main/java/de/adorsys/android/smsparsertest/MainActivity.java
+++ app/src/main/java/de/adorsys/android/smsparsertest/MainActivity.java
@@ -22,11 +22,11 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @NonNull
     private LocalBroadcastManager localBroadcastManager;
 
-    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(SmsReceiver.INTENT_ACTION_SMS)) {
@@ -60,16 +60,14 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String permissions[],
                                            @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case SmsTool.REQUEST_CODE_ASK_PERMISSIONS:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length <= 0
-                        || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, getString(R.string.warning_permission_not_granted),
-                            Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                            Uri.parse("package:" + getApplicationContext().getPackageName())));
-                }
+        // If request is cancelled, the result arrays are empty.
+        if (requestCode == SmsTool.REQUEST_CODE_ASK_PERMISSIONS
+                && grantResults.length <= 0
+                || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, getString(R.string.warning_permission_not_granted),
+                    Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + getApplicationContext().getPackageName())));
         }
     }
 
