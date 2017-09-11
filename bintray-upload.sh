bbd6d9e7e3a9f541589d1effbb8adc6ad44faea6
Drilon Reçica <drilonreqica@gmail.com>
2017-09-11 10:21:22 +0200

    Updated bintray-upload.sh to update library only on master branch

--- bintray-upload.sh
+++ bintray-upload.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-echo -e "Start clean module"
-./gradlew :smsparser:clean
-echo -e "Finished clean"
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo -e "Start clean module"
+    ./gradlew :smsparser:clean
+    echo -e "Finished clean"
 
-echo -e "Start install module"
-./gradlew :smsparser:install
-echo -e "Finished install"
+    echo -e "Start install module"
+    ./gradlew :smsparser:install
+    echo -e "Finished install"
 
-echo -e "Start bintrayUpload"
-./gradlew :smsparser:bintrayUpload
-echo -e "Finished bintrayUpload"
+    echo -e "Start bintrayUpload"
+    ./gradlew :smsparser:bintrayUpload
+    echo -e "Finished bintrayUpload"
+else
+   echo "Current branch is not master, will not upload to bintray."
+fi
