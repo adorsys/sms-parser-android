6869fca0d60316b4c2ce6f1e7ea1b323ff1d23e3
Drilon Reçica <drilonreqica@gmail.com>
2017-09-11 10:34:36 +0200

    Color echo messages differently in console for better readability

--- bintray-upload.sh
+++ bintray-upload.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    echo -e "Start clean module"
+    echo -e "\033[0;32m Start clean module \033[0m"
     ./gradlew :smsparser:clean
-    echo -e "Finished clean"
+    echo -e "\033[0;32m Finished clean \033[0m"
 
-    echo -e "Start install module"
+    echo -e "\033[0;32m Start install module \033[0m"
     ./gradlew :smsparser:install
-    echo -e "Finished install"
+    echo -e "\033[0;32m Finished install \033[0m"
 
-    echo -e "Start bintrayUpload"
+    echo -e "\033[0;32m Start bintrayUpload \033[0m"
     ./gradlew :smsparser:bintrayUpload
-    echo -e "Finished bintrayUpload"
+    echo -e "\033[0;32m Finished bintrayUpload \033[0m"
 else
-   echo "Current branch is not master, will not upload to bintray."
+   echo "\033[0;32m Current branch is not master, will not upload to bintray. \033[0m"
 fi
