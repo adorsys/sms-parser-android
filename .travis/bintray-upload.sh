f130270452ae8679e58fffb124b0ea6dcf6db73a
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-09-01 13:45:50 +0200

    Update travis process

--- .travis/bintray-upload.sh
+++ .travis/bintray-upload.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo -e "\033[0;32m ./gradlew :smsparser:clean \033[0m"
-./gradlew :smsparser:clean
+../gradlew :smsparser:clean
 
 echo -e "\033[0;32m ./gradlew :smsparser:install \033[0m"
-./gradlew :smsparser:install
+../gradlew :smsparser:install
 
 if [ "$CI" == true ] && [ "$TRAVIS_PULL_REQUEST" == false ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
     echo -e "\033[0;32m ./gradlew :smsparser:bintrayUpload \033[0m"
-    ./gradlew :smsparser:bintrayUpload
+    ../gradlew :smsparser:bintrayUpload
 
 else
    echo -e "\033[0;32m Current branch is not master, will not upload to bintray. \033[0m"
